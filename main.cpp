f8fdbd84fca05cdd810883db15c8d7aee8e1e2cb
andrei811 <61988494+andrei811@users.noreply.github.com>
2021-04-28 19:31:55 +0300

    Illuminance added ;)

--- main.cpp
+++ main.cpp
@@ -12,9 +12,62 @@
 
 using namespace std;
 
-char gray_scale[] = ".':+*&#@";
+struct vect{
+    double x, y, z;
+    friend ostream& operator<<(ostream& out, const vect& a)
+    {
+        out << fixed << setprecision(2) << "{" << a.x << "," << a.y << ", " << a.z << "}";
+        return out;
+    }
+    double operator*(const vect& a)
+    {
+        double aux = a.x * x + a.x * y + a.x * z + a.y * x + a.y * y + a.y * z + a.z * x + a.z * y + a.z * z;
+        return aux;
+    }
+}Normals[SPACE_DIM][SPACE_DIM][SPACE_DIM], light, rotNormals[SPACE_DIM][SPACE_DIM][SPACE_DIM];
+
+// ostream& operator<<(ofstream& out, vect a)
+
+char gray_scale[] = ".:*#@";
 char space[SPACE_DIM][SPACE_DIM][SPACE_DIM];
 char rotMat[SPACE_DIM][SPACE_DIM][SPACE_DIM], displayMat[SPACE_DIM][SPACE_DIM];
+
+char findLum(double val)
+{
+    if ( val < 0 )
+        return gray_scale[0];
+    if ( val < 1 )
+        return gray_scale[1];
+    if ( val < 3 )
+        return gray_scale[2];
+    if ( val < 5 )
+        return gray_scale[3];
+    return gray_scale[4];
+}
+
+vect NormVect(double x, double y, double z)
+{
+    double aux = sqrt(x * x + y * y), len = sqrt(x * x + y * y + z * z);
+
+    if ( aux == 0 )
+    {
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+        cout << "ERROR: ";
+        // system("Color 07");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+        cout << "DIVIDING BY 0 IN 'NormVect' FUNCTION\n";
+        exit(EXIT_FAILURE);
+    }
+
+    return { 2.0 * x * (R - aux) / (aux * len), 2.0 * y * (R - aux) / (aux * len), 1 / len };
+}
+
+int roundDouble(double x)
+{
+    if ( x <= 0.5 )
+        return ( int )x;
+    return ( int )x + 1;
+}
 
 void copyMatrix(double A[4][4], double B[4][4])
 {
@@ -95,22 +148,30 @@ void createTorus()
     for ( int i = 0; i <= X_DIM; i++ )
         for ( int j = 0; j <= Y_DIM; j++ )
             for ( int k = 0; k <= Z_DIM; k++ )
-                if ( formula(i + X_BIAS, j + Y_BIAS, k + Z_BIAS) <= (1.0 * r * r) )
-                    space[i][j][k] = gray_scale[7];
+                if ( round(formula(i + X_BIAS, j + Y_BIAS, k + Z_BIAS)) <= (1.0 * r * r) )
+                {
+                    space[i][j][k] = gray_scale[0];
+                    Normals[i][j][k] = NormVect(i + X_BIAS, j + Y_BIAS, k + Z_BIAS);
+                }
                 else
                     space[i][j][k] = ' ';
+}
+
+void FindSurfaceNormal(int x, int y, int z)
+{
+    int nudge = 0.25;
+    int newx, newy;
+    int sign = 1;
+
+    if ( formula(x + nudge, y, z) > 1.0 * r * r )
+        sign = -1;
+
+
 }
 
 inline bool inside_space(double v[4])
 {
     return !(v[1] < 0 || v[2] < 0 || v[3] < 0 || v[1] > X_DIM || v[2] > Y_DIM || v[3] > Z_DIM);
-}
-
-int roundDouble(double x)
-{
-    if ( x <= 0.5 )
-        return ( int )x;
-    return ( int )x + 1;
 }
 
 /*
@@ -125,8 +186,6 @@ void rotate(double radAngle, int axis)
 
     double v[4] = { 0, 0, 0, 0 };
     double Rotation[4][4] = { {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0} };
-
-    int a = 0;
 
     if ( axis == 3 )
         M_INV_X(Rotation, radAngle);
@@ -167,6 +226,16 @@ void rotate(double radAngle, int axis)
                 if ( !inside_space(v) )
                     continue;
                 rotMat[i][j][k] = space[roundDouble(v[1])][roundDouble(v[2])][roundDouble(v[3])];
+                rotNormals[i][j][k] = Normals[roundDouble(v[1])][roundDouble(v[2])][roundDouble(v[3])];
+
+                v[1] = rotNormals[i][j][k].x;
+                v[2] = rotNormals[i][j][k].y;
+                v[3] = rotNormals[i][j][k].z;
+
+                matVectMult(v, Rotation);
+                rotNormals[i][j][k].x = v[1];
+                rotNormals[i][j][k].y = v[2];
+                rotNormals[i][j][k].z = v[3];
             }
 }
 
@@ -179,8 +248,11 @@ void transform_2d()
     for ( int k = 0; k <= Z_DIM; k++ )
         for ( int i = 0; i <= X_DIM; i++ )
             for ( int j = 0; j <= Y_DIM; j++ )
-                if ( displayMat[i][j] == ' ' )
-                    displayMat[i][j] = rotMat[i][j][k];
+                if ( displayMat[i][j] == ' ' && rotMat[i][j][k] != ' ' )
+                {
+                    // displayMat[i][j] = rotMat[i][j][k];
+                    displayMat[i][j] = findLum(( double )(rotNormals[i][j][k] * light));
+                }
 }
 
 void clearscreen()
@@ -223,6 +295,7 @@ void update()
 
 int main()
 {
+    light.x = light.y = 3;
     createTorus();
 
     clearscreen();
@@ -236,9 +309,20 @@ int main()
     // {
     //     for ( int j = 0; j <= Y_DIM; j++ )
     //         // for ( int k = 0; k <= Y_DIM; k++ )
-    //         cout << displayMat[i][j];
+    //         cout << space[i][j][5];
     //     cout << "\n";
     // }
+    // cout << endl;
+    // cout << endl;
+    // double minn = INT_MAX;
+
+    // for ( int i = 0; i <= X_DIM; i++ )
+    // {
+    //     for ( int j = 0; j <= Y_DIM; j++ )
+    //         for ( int k = 0; k <= Y_DIM; k++ )
+    //             minn = min(minn, ( double )(Normals[i][j][k] * light));
+    // }
+    // cout << minn;
     // return 0;
 
     double angle = 0;
@@ -247,7 +331,7 @@ int main()
     {
         if ( angle >= 6.28 )
             angle = 0;
-        rotate(angle, 5);
+        rotate(angle, 1);
         update();
         angle += 0.1;
         // Sleep(1000);
